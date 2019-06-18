497f4de705c3331e7d1c9cbe67e5affc3614f3fb
Max Josino <max.josino@gmail.com>
2019-06-18 12:28:35 +0200

    updating

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-
 #!/usr/bin/env bash
 brew install git
 brew install git-flow
